2f11739ef9a5abd672de6fcec655bdee1e63b537
januka36 <janukashehan@gmail.com>
2023-10-13 11:23:32 +0000

    Application connected with database

--- database.py
+++ database.py
@@ -40,3 +40,17 @@ def load_job_from_db(id):
 #       print("None")
 #     else:
 #       print(dict(zip(column_names, rows[val])))
+
+def add_application_to_db(job_id, data):
+  with engine.connect() as conn:
+    query = text("INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)")
+
+    conn.execute(query, 
+      parameters=dict(job_id = job_id,
+      full_name=data["full_name"],
+      email=data["email"],
+      linkedin_url=data["linkedin_url"],
+      education=data["education"],
+      work_experience=data["work_experience"],
+      resume_url=data["resume_url"]
+      ))
